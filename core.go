fb1adb667984bcdcf58580c8b0120c1b4feb6b06
S <S@mail.minecraft.jp.net>
2015-11-23 15:25:33 +0900

    Update: Furnaceの画像作成ができるように

--- core.go
+++ core.go
@@ -17,13 +17,18 @@ func main() {
 			if recipeType.Type == "craftingTable" {
 				craftingTable(&recipeType.Recipes, &config)
 			}
+			if recipeType.Type == "furnace" {
+				furnace(&recipeType.Recipes, &config)
+			}
 		}
 	}
 }
 
 func craftingTable(recipes *[]recipeReader.Recipe, config *cfgReader.Config) {
-	layerImg := load.Load("cfg/" + config.CraftingTable.Gui)
-	place := config.CraftingTable.Place
+	cfg := config.CraftingTable
+
+	layerImg := load.Load("cfg/" + cfg.Gui)
+	place := cfg.Place
 
 	rs := *recipes
 	for _, recipe := range rs {
@@ -37,7 +42,31 @@ func craftingTable(recipes *[]recipeReader.Recipe, config *cfgReader.Config) {
 			}
 		}
 
-		lImg = edit.TrimArr(&lImg, config.CraftingTable.Trim)
-		output.Output("output/" + recipe.Img[recipe.Shape[9]] + ".png", &lImg)
+		lImg = edit.TrimArr(&lImg, cfg.Trim)
+		output.Output("output/" + recipe.Img[recipe.Shape[len(shape) - 1]] + ".png", &lImg)
+	}
+}
+
+func furnace(recipes *[]recipeReader.Recipe, config *cfgReader.Config) {
+	cfg := config.Furnace
+
+	layerImg := load.Load("cfg/" + cfg.Gui)
+	place := cfg.Place
+
+	rs := *recipes
+	for _, recipe := range rs {
+		lImg := layerImg
+
+		for i, item := range recipe.Shape {
+			if item != "" {
+				imgPath := "assets/" + recipe.Img[item] + ".png"
+				img := load.Load(imgPath)
+				edit.PasteArr(&lImg, place[i], &img)
+			}
+		}
+
+		lImg = edit.TrimArr(&lImg, cfg.Trim)
+		shape := recipe.Shape
+		output.Output("output/" + recipe.Img[shape[len(shape) - 1]] + ".png", &lImg)
 	}
 }
