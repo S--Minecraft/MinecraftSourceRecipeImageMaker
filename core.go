1f67c2052afc6b95cd1b241836d90600c1d51cbc
S <S@mail.minecraft.jp.net>
2015-11-23 17:05:41 +0900

    Ref: リファクタリング

--- core.go
+++ core.go
@@ -17,12 +17,13 @@ func main() {
 
 	for _, recipe := range allRecipe {
 		for _, recipeType := range recipe.RecipeType {
-			readRecipe(&configs, &layerImgs, &recipeType)
+			readTypes(&configs, &layerImgs, &recipeType)
 		}
 	}
+	return
 }
 
-func readRecipe(configs *map[string]cfgReader.Config, layerImgs *map[string]image.Image, recipeType *recipeReader.RecipeType) {
+func readTypes(configs *map[string]cfgReader.Config, layerImgs *map[string]image.Image, recipeType *recipeReader.RecipeType) {
 	// 対応config読み込み(読み込み済みでない場合ファイルロード)
 	cfgs := *configs
 	config, ok := cfgs[recipeType.Type]
@@ -43,23 +44,31 @@ func readRecipe(configs *map[string]cfgReader.Config, layerImgs *map[string]imag
 		layer = edit.TrimArr(&layer, config.Trim)
 		lImgs[recipeType.Type] = layer
 	}
-	makeImg(&recipeType.Recipes, &config, layer)
+	readRecipe(&recipeType.Recipes, &config, &layer)
+	return
 }
 
-func makeImg(recipes *[]recipeReader.Recipe, cfg *cfgReader.Config, layerImg image.Image) {
+func readRecipe(recipes *[]recipeReader.Recipe, cfg *cfgReader.Config, layer *image.Image) {
 	place := cfg.Place
 
 	rs := *recipes
 	for _, recipe := range rs {
-		for i, item := range recipe.Shape {
-			if item != "" {
-				imgPath := "assets/" + recipe.Img[item] + ".png"
-				img := load.Load(imgPath)
-				edit.PasteArrOffset(&layerImg, place[i], cfg.Trim, &img)
-			}
-		}
-
-		shape := recipe.Shape
-		output.Output("output/" + recipe.Img[shape[len(shape) - 1]] + ".png", &layerImg)
+		makeImg(&recipe, cfg, *layer, &place)
 	}
+	return
+}
+
+func makeImg(recipe *recipeReader.Recipe, cfg *cfgReader.Config, layerImg image.Image, placeP *[][]int) {
+	place := *placeP
+
+	for i, item := range recipe.Shape {
+		if item != "" {
+			imgPath := "assets/" + recipe.Img[item] + ".png"
+			img := load.Load(imgPath)
+			edit.PasteArrOffset(&layerImg, place[i], cfg.Trim, &img)
+		}
+	}
+	shape := recipe.Shape
+	output.Output("output/" + recipe.Img[shape[len(shape) - 1]] + ".png", &layerImg)
+	return
 }
